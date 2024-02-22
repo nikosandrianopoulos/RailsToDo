48ae33db1fce73b4fe974bad6239c19d997148c1
Panagiotis Kriparakos <73951066+panoskrip@users.noreply.github.com>
2024-02-22 15:46:33 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
 end
