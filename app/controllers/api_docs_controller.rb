4fc32e372c9cb2877988647a7a76f34609f9288f
Nicolas Andrianopoulos <nikosandrianopoulos02@gmail.com>
2024-02-23 18:20:15 +0200

    changes on yaml

--- app/controllers/api_docs_controller.rb
+++ app/controllers/api_docs_controller.rb
@@ -1,13 +1,13 @@
-class ApiDocsController < ApplicationController
-    skip_before_action :authorize_request, only: :show
-    def show
-        file_path = Rails.root.join('config', 'api_docs', 'openapi.yaml')
+# class ApiDocsController < ApplicationController
+#     skip_before_action :authorize_request, only: :show
+#     def show
+#         file_path = Rails.root.join('config', 'api_docs', 'openapi.yaml')
       
-        if File.exist?(file_path)
-          yaml_content = File.read(file_path)
-          render plain: yaml_content
-        else
-          render json: { error: 'File not found' }, status: :not_found
-        end
-      end
-end
+#         if File.exist?(file_path)
+#           yaml_content = File.read(file_path)
+#           render plain: yaml_content
+#         else
+#           render json: { error: 'File not found' }, status: :not_found
+#         end
+#       end
+# end
