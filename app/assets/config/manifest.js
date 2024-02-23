c263e51e40c770e86ce6ff07e3415df618287f0b
Nicolas Andrianopoulos <nikosandrianopoulos02@gmail.com>
2024-02-23 18:26:07 +0200

    restored manifest.js

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,21 @@
+// window.onload = function() {
+//     // Customize the Swagger UI appearance
+//     const ui = window.ui;
+  
+//     // Change the colors
+//     ui.colors.primary = '#003366'; 
+//     ui.colors.success = '#4CAF50'; 
+//     ui.colors.failure = '#FF5252'; 
+  
+//     // Customize the layout
+//     ui.initOAuth({
+//       appName: "Your App Name",
+//       clientId: "your-client-id",
+//       realm: "your-realm",
+//       scopeSeparator: " ",
+//       scopes: "scope1 scope2",
+//       additionalQueryStringParams: {}
+//     });
+  
+//   };
+  
--- config/manifest.js
+++ config/manifest.js
@@ -1,3 +1,11 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_directory ../javascripts .js
+window.onload = function() {
+    // Customize the Swagger UI appearance
+    const ui = window.ui;
+  
+    // Change the colors
+    ui.colors.primary = '#336699'; // Change primary color
+    ui.colors.success = '#4CAF50'; // Change success color
+    ui.colors.failure = '#FF5252'; // Change failure color
+  
+  };
+  
